3612873e31c3594b17eb2ea9f5d23cdce4a942dd
pentamorfico <amarrod96@gmail.com>
2024-01-31 08:28:22 +0100

    Add batch of files

--- script.bash
+++ script.bash
@@ -3,27 +3,23 @@
 
 BATCH_SIZE=100  # Number of files to process in each batch
 COUNT=0
-BATCH=()
 
-while IFS= read -r -d $'\0' FILE; do
-    BATCH+=("$FILE")
+find . -type f -not -path "./.git/*" -print0 | while IFS= read -r -d $'\0' FILE; do
+    git add "$FILE"
     let COUNT+=1
-    echo $BATCH
+
     if [[ $COUNT -eq $BATCH_SIZE ]]; then
-        git add "${BATCH[@]}"
         git commit -m "Add batch of files"
         git push origin main  # Replace 'main' with your branch name
         echo "Batch committed and pushed."
         
-        # Reset for next batch
+        # Reset count for the next batch
         COUNT=0
-        BATCH=()
     fi
-done < <(find . -type f -not -path "./.git/*" -print0)
+done
 
 # Process any remaining files
 if [[ $COUNT -ne 0 ]]; then
-    git add "${BATCH[@]}"
     git commit -m "Add batch of files"
     git push origin main  # Replace 'main' with your branch name
     echo "Final batch committed and pushed."
